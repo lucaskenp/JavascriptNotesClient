f7fb3de36c227daa46e0d9353b854d07c58ef8d2
Lucas Lima <lucasmonteirolima17@gmail.com>
2021-09-02 22:08:35 -0300

    Conectando o login com a API

--- src/components/auth/login_form/index.js
+++ src/components/auth/login_form/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import { Button, Field, Control, Input, Column, Section, Help, Label } from "rbx";
 import { Redirect } from "react-router-dom";
+import UsersService from '../../../services/users';
 
 function LoginForm() {
     const [email, setEmail] = useState("");
@@ -8,6 +9,16 @@ function LoginForm() {
     const [redirectToRegister, setRedirectToRegister] = useState(false);
     const [redirectToNotes, setRedirectToNotes] = useState(false);
     const [error, setError] = useState(false);
+    
+    const HandleSubmit = async (evt) => {
+        evt.preventDefault();
+        try {
+            const user = await UsersService.login({email: email, password: password});
+            setRedirectToNotes(true);
+        } catch (error) {
+            setError(true);
+        }
+    }
 
     if (redirectToRegister)
         return <Redirect to={{ pathname: "/register" }} />
@@ -17,7 +28,7 @@ function LoginForm() {
     return (
         <Fragment>
             <Column.Group centered>
-                <form>
+                <form onSubmit={HandleSubmit}>
                     <Column size={12}>
                         <Field>
                             <Label size="small">Email:</Label>
@@ -50,7 +61,7 @@ function LoginForm() {
                                         <a onClick={e => setRedirectToRegister(true)} className="button is-white has-text-custom-purple">Register or</a>
                                     </Column>
                                     <Column>
-                                        <Button onClick={e => setRedirectToNotes(true)} color="custom-purple" outlined>Login</Button>
+                                        <Button  color="custom-purple" outlined>Login</Button>
                                     </Column>
                                 </Column.Group>
                             </Control>
