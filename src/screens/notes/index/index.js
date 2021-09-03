3258ac232bf6ac7b7a41764832b610f224215128
Lucas Lima <lucasmonteirolima17@gmail.com>
2021-09-03 09:56:25 -0300

    Sidebar(notas)

--- src/screens/notes/index/index.js
+++ src/screens/notes/index/index.js
@@ -1,10 +1,15 @@
-import React, { Fragment } from 'react';
-import HeaderLogged from "../../../components/header_logged";
+import React, { Fragment, useState } from 'react';
+import HeaderLogged from "../../../components/header_logged/index";
+import Notes from "../../../components/notes";
 
-const NotesScreen = () => (
-  <Fragment>
-    <HeaderLogged/>
-  </Fragment>
-);
+function NotesScreen() {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <Fragment>
+            <HeaderLogged setIsOpen={setIsOpen} />
+            <Notes setIsOpen={setIsOpen} isOpen={isOpen} />
+        </Fragment>
+    )
+}
 
 export default NotesScreen;
